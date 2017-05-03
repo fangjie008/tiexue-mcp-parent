1bb6e84d633c0a3213711f16222f4a8f13dc3e97
zhangxiaowei <zhangxiaowei@tiexue.com>
2017-05-03 15:50:52 +0800

    添加监听

--- mcp-manage/src/main/java/com/tiexue/mcp/manage/controller/McpBaseInfoController.java
+++ mcp-manage/src/main/java/com/tiexue/mcp/manage/controller/McpBaseInfoController.java
@@ -1,24 +1,28 @@
 package com.tiexue.mcp.manage.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tiexue.mcp.core.service.IMcpBaseInfoService;
 @Controller
 @RequestMapping("mcpbaseinfo")
 public class McpBaseInfoController {
 	//日志
 	private static Logger logger=Logger.getLogger(McpBaseInfoController.class);
 	
-	
+	@Resource
+	IMcpBaseInfoService mcpBaseInfoService;
 	
 	
 	/**
 	 * 获取基础信息
 	 */
-	@RequestMapping("model")
+	@RequestMapping("/model")
 	public String getbaseInfo(HttpServletRequest request,HttpServletResponse response){
 		return "mcpBaseInfo/model";
 	}
@@ -26,7 +30,7 @@ public class McpBaseInfoController {
 	/**
 	 * 获取基础信息
 	 */
-	@RequestMapping("add")
+	@RequestMapping("/add")
 	public String addbaseInfo(HttpServletRequest request,HttpServletResponse response){
 		return "mcpBaseInfo/add";
 	}
@@ -35,7 +39,7 @@ public class McpBaseInfoController {
 	/**
 	 * 获取基础信息
 	 */
-	@RequestMapping("show")
+	@RequestMapping("/show")
 	public String showbaseInfo(HttpServletRequest request,HttpServletResponse response){
 		return "mcpBaseInfo/show";
 	}
